73ffe6cc2c56c03f0bce3e9350db6d12e22e293e
Radon Rosborough <radon.neon@gmail.com>
2020-11-03 10:03:00 -0800

    Configure Webpack to transpile TypeScript

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+function isProduction(argv) {
+  return !argv.development;
+}
+
+module.exports = (_, argv) => ({
+  devtool: isProduction(argv) ? undefined : "source-map",
+  entry: {
+    index: "./frontend/js/src/index.ts",
+  },
+  mode: isProduction(argv) ? "production" : "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/i,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "frontend/js/out/"),
+    publicPath: "/js/",
+    filename: "[name].js",
+  },
+});
