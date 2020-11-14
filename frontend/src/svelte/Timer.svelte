870580e832cf0ef579aa0ab79bc7a57050e2a4e5
Radon Rosborough <radon.neon@gmail.com>
2020-11-14 14:15:48 -0800

    Run everything through Prettier

--- frontend/src/svelte/Timer.svelte
+++ frontend/src/svelte/Timer.svelte
@@ -1,35 +1,39 @@
 <script>
-    import { tweened } from 'svelte/motion';
-    export let endTime;
-    export let startTime;
-    export let api;
-    let endDate = new Date(endTime);
-    let startDate = new Date(startTime);
-    let now = new Date();
-    let originalLength = (endDate.getTime() - startDate.getTime())/1000;
-    let timeLeft = (endDate.getTime() - now.getTime())/1000;
+  import { tweened } from "svelte/motion";
+  export let endTime;
+  export let startTime;
+  export let api;
+  let endDate = new Date(endTime);
+  let startDate = new Date(startTime);
+  let now = new Date();
+  let originalLength = (endDate.getTime() - startDate.getTime()) / 1000;
+  let timeLeft = (endDate.getTime() - now.getTime()) / 1000;
 
-    let timer = tweened(timeLeft)
+  let timer = tweened(timeLeft);
   let interval = setInterval(() => {
-      if ($timer > 0) {
-          $timer--;
-      } else {
-          gameOver();
-      }
+    if ($timer > 0) {
+      $timer--;
+    } else {
+      gameOver();
+    }
   }, 1000);
 
   $: minutes = Math.floor($timer / 60);
   $: minname = minutes > 1 ? "mins" : "min";
-  $: seconds = Math.floor($timer - minutes * 60)
- function gameOver(){
-     clearInterval(interval);
-     let message = {
-         event: "stopGame"
-     };
-     api.socket.send(JSON.stringify(message));
- }
+  $: seconds = Math.floor($timer - minutes * 60);
+  function gameOver() {
+    clearInterval(interval);
+    let message = {
+      event: "stopGame",
+    };
+    api.socket.send(JSON.stringify(message));
+  }
 </script>
+
 <main>
-<h1><span class="mins">{minutes}</span>:<span class="secs">{seconds}</span> !</h1>
-<progress value={$timer/originalLength}></progress>
+  <h1>
+    <span class="mins">{minutes}</span>:<span class="secs">{seconds}</span>
+    !
+  </h1>
+  <progress value={$timer / originalLength} />
 </main>
--- frontend/src/svelte/index.svelte
+++ frontend/src/svelte/index.svelte
@@ -2,11 +2,11 @@
   import "./index/global.svelte";
 
   import { API } from "./index/api.js";
-  import { v4 as uuidv4 } from 'uuid';
-  import Timer from './Timer.svelte'
-  import Lobby from './Lobby.svelte'
-  import Config from './Config.svelte'
-  import Submission from './Submission.svelte'
+  import { v4 as uuidv4 } from "uuid";
+  import Timer from "./Timer.svelte";
+  import Lobby from "./Lobby.svelte";
+  import Config from "./Config.svelte";
+  import Submission from "./Submission.svelte";
 
   let state = {
     game: {
@@ -14,8 +14,8 @@
     },
   };
   let config = false;
- let playerID = getPlayerID();
- let composedMessages = {};
+  let playerID = getPlayerID();
+  let composedMessages = {};
 
   const api = new API({
     onStateUpdate: (newState) => {
@@ -26,70 +26,97 @@
   });
   api.connect();
 
- function toggleConfig(){
-     config = !config
- }
- function getPlayerIDFromCookie(){
-     let currentID = document.cookie
-                             .split('; ')
-                             .find(row => row.startsWith("playerID"))
-     if (currentID){
-        return currentID.split('=')[1];
-     }
-     return null
- }
+  function toggleConfig() {
+    config = !config;
+  }
+  function getPlayerIDFromCookie() {
+    let currentID = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("playerID"));
+    if (currentID) {
+      return currentID.split("=")[1];
+    }
+    return null;
+  }
 
- // Reads playerID from the cookie. Sets a random one if it doesn't exist
- function getPlayerID() {
-     let currentID = getPlayerIDFromCookie()
-     if (currentID) {
-         return currentID
-     }
-     let newUUID = "player-" + uuidv4()
-     document.cookie = "playerID=" + newUUID + "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
-     return newUUID
- }
- function goToLobby(){
-     let message = {
-         event: "startPregame"
-     };
-     api.socket.send(JSON.stringify(message));
- }
- function checkForSend(groupID){
-     if (event.code == 'Enter'){
-         console.log("sending", composedMessages[groupID], "to", groupID);
-         sendMessage(groupID, state.players[playerID].node, composedMessages[groupID])
-         composedMessages[groupID] = "";
-     }
- }
- function sendMessage(groupID, sender, text){
-     let message = {
-         event: "sendMessage",
-         groupID: groupID,
-         sender: sender,
-         text: text
-     }
-     api.socket.send(JSON.stringify(message));
- }
+  // Reads playerID from the cookie. Sets a random one if it doesn't exist
+  function getPlayerID() {
+    let currentID = getPlayerIDFromCookie();
+    if (currentID) {
+      return currentID;
+    }
+    let newUUID = "player-" + uuidv4();
+    document.cookie =
+      "playerID=" + newUUID + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    return newUUID;
+  }
+  function goToLobby() {
+    let message = {
+      event: "startPregame",
+    };
+    api.socket.send(JSON.stringify(message));
+  }
+  function checkForSend(groupID) {
+    if (event.code == "Enter") {
+      console.log("sending", composedMessages[groupID], "to", groupID);
+      sendMessage(
+        groupID,
+        state.players[playerID].node,
+        composedMessages[groupID]
+      );
+      composedMessages[groupID] = "";
+    }
+  }
+  function sendMessage(groupID, sender, text) {
+    let message = {
+      event: "sendMessage",
+      groupID: groupID,
+      sender: sender,
+      text: text,
+    };
+    api.socket.send(JSON.stringify(message));
+  }
 </script>
 
 <main>
   {#if config}
-      <Config players={state.players} availableRoles={state.possibleRoles} toggleConfig={toggleConfig} api={api} graph={state.graph} facts={state.facts} possibleFacts={state.possibleFacts}/>
+    <Config
+      players={state.players}
+      availableRoles={state.possibleRoles}
+      {toggleConfig}
+      {api}
+      graph={state.graph}
+      facts={state.facts}
+      possibleFacts={state.possibleFacts} />
   {:else if state.game.state === 'loading'}
     Loading...
   {:else if state.game.state === 'lobby'}
-      <Lobby players={state.players} api={api} playerID={playerID} toggle={toggleConfig}/>
+    <Lobby players={state.players} {api} {playerID} toggle={toggleConfig} />
   {:else if state.game.state === 'submission'}
-      <Submission player={state.players[playerID]} api={api} toggle={toggleConfig} facts={state.facts}/>
+    <Submission
+      player={state.players[playerID]}
+      {api}
+      toggle={toggleConfig}
+      facts={state.facts} />
   {:else if state.game.state === 'playing'}
     <div class="columns is-gapless">
       <div class="column is-three-quarters">
         <div class="rows" style="height: 100vh">
           <div class="row" style="height: 20%">
             <div class="columns is-gapless">
-              <div class="column">Name: {state.players[playerID].name} and role: {state.players[playerID].role}</div>
-              <div class="column">Timer: <Timer startTime={state.game.startTime} endTime={state.game.stopTime} api={api}/></div>
+              <div class="column">
+                Name:
+                {state.players[playerID].name}
+                and role:
+                {state.players[playerID].role}
+              </div>
+              <div class="column">
+                Timer:
+                <Timer
+                  startTime={state.game.startTime}
+                  endTime={state.game.stopTime}
+                  {api} />
+              </div>
             </div>
           </div>
           <div class="row" style="height: 80%">
@@ -101,14 +128,18 @@
                   {#if state.graph.groups[groupID]}
                     <div class="column">
                       <div class="rows">
-                          <div class="row">
-                              Conversation with: TODO
-                          </div>
-                          <div class="row">
+                        <div class="row">Conversation with: TODO</div>
+                        <div class="row">
                           {#each state.graph.groups[groupID].messages as { sender, text }}
-                            <p style="color: {state.graph.nodes[sender].color}">{state.graph.nodes[sender].name}: {text}</p>
+                            <p style="color: {state.graph.nodes[sender].color}">
+                              {state.graph.nodes[sender].name}:
+                              {text}
+                            </p>
                           {/each}
-                          <input type="text" on:keyup|preventDefault={()=>checkForSend(groupID)} bind:value={composedMessages[groupID]}>
+                          <input
+                            type="text"
+                            on:keyup|preventDefault={() => checkForSend(groupID)}
+                            bind:value={composedMessages[groupID]} />
                         </div>
                       </div>
                     </div>
@@ -147,8 +178,8 @@
         </div>
       </div>
     </div>
-    {:else if state.game.state === 'results'}
-      TODO: implement results page
-      <button on:click={goToLobby}> Return To Lobby </button>
-    {/if}
+  {:else if state.game.state === 'results'}
+    TODO: implement results page
+    <button on:click={goToLobby}> Return To Lobby </button>
+  {/if}
 </main>
