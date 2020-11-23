cfe2c81edf0259ad0da1a217a3b7356ca03aeda4
Radon Rosborough <radon.neon@gmail.com>
2020-11-22 17:28:11 -0800

    Prettier

--- frontend/src/svelte/Graph.svelte
+++ frontend/src/svelte/Graph.svelte
@@ -16,31 +16,31 @@
     for (const [nodeID, { groups: groupIDs }] of Object.entries(
       stateGraph.nodes
     )) {
-        // console.log("add node", nodeID);
-        // console.log(s.graph.nodes());
-        var x;
-        var y;
-        if (nodeID.length < 10){
-            console.log(nodeID)
-            let num = parseInt(nodeID.slice(5), 10);
-            x = Math.cos(num / 6 * 2 * Math.PI);
-            y = Math.sin(num / 6 * 2 * Math.PI);
-        } else {
-            x = Math.random();
-            y = Math.random();
-        }
+      // console.log("add node", nodeID);
+      // console.log(s.graph.nodes());
+      var x;
+      var y;
+      if (nodeID.length < 10) {
+        console.log(nodeID);
+        let num = parseInt(nodeID.slice(5), 10);
+        x = Math.cos((num / 6) * 2 * Math.PI);
+        y = Math.sin((num / 6) * 2 * Math.PI);
+      } else {
+        x = Math.random();
+        y = Math.random();
+      }
       s.graph.addNode({
         id: nodeID,
         label: stateGraph.nodes[nodeID].name,
         x: x,
         y: y,
         size: 1,
-        color: "#f00",
+        color: stateGraph.nodes[nodeID].color,
       });
       for (const groupID of groupIDs) {
-          if (groupID === "group-baddies"){
-              continue;
-          }
+        if (groupID === "group-baddies") {
+          continue;
+        }
         let groupNodeIDs = edges.get(groupID);
         if (!groupNodeIDs) {
           groupNodeIDs = new Set();
@@ -51,18 +51,19 @@
     }
     for (const [groupID, groupNodeIDs] of edges.entries()) {
       if (groupNodeIDs.size !== 2) {
-          // console.log(
-          //   `Graph rendering: skipping hyper-edge of size ${groupNodeIDs.size}:`,
-          //   groupID
-          // );
+        // console.log(
+        //   `Graph rendering: skipping hyper-edge of size ${groupNodeIDs.size}:`,
+        //   groupID
+        // );
         continue;
       }
       const [source, target] = Array.from(groupNodeIDs);
-        // console.log("add edge", groupID, `[${source} -> ${target}]`);
+      // console.log("add edge", groupID, `[${source} -> ${target}]`);
       s.graph.addEdge({
         id: groupID,
         source,
         target,
+        color: "#000",
       });
     }
     s.refresh();
@@ -71,7 +72,7 @@
   $: updateGraph(sigmaGraph, stateGraph);
 
   onMount(() => {
-      // console.log("container:", container);
+    // console.log("container:", container);
     sigmaGraph = new sigma({
       container,
     });
